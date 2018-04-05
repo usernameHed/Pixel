5844ee6428602343d2a9b45a17735b07292c6b65
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-04-05 19:51:04 +0200

    Ajout de fmod

---
+++ Assets/_Scripts/Core/_Main/ListenerPosition.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+
+/// <summary>
+/// ListenerPosition Description
+/// </summary>
+public class ListenerPosition : MonoBehaviour
+{
+    #region Attributes
+
+    #endregion
+
+    #region Initialization
+
+    private void Start()
+    {
+		// Start function
+    }
+    #endregion
+
+    #region Core
+
+    #endregion
+
+    #region Unity ending functions
+
+    private void LateUpdate()
+    {
+        transform.SetZ(0);
+    }
+
+
+    #endregion
+}
